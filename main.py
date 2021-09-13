201c0012f29008c2e27e50c7506bf9904c791772
Devansh Ramdurgekar <devanshr3012@gmail.com>
2021-09-13 20:18:56 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template,request
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app= Flask('__name__')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -11,20 +12,13 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     username = db.Column(db.String(80), unique = True, nullable = False)
     email = db.Column(db.String(120),unique=True, nullable = False)
+    datetimer=db.Column(db.DateTime)
 
     def __repr__(self):
-        return f'{self.id} - {self.username} - {self.email}'
+        return f'{self.id} - {self.username} - {self.email} - {self.datetimer}'
 
-def datataker():
-    idcard = int(input("Enter ID "))
-    usernamed = input("Select Username ")
-    emailed = input("Enter email ")
-    value = User(id=idcard, username = usernamed, email=emailed)
-    db.session.add(value)
-    db.session.commit()
-
-def dataentry(Identity, UserID, ElectronicMail):
-    db.session.add(User(id=Identity, username= UserID, email = ElectronicMail))
+def dataentry(Identity, UserID, ElectronicMail,Dated=datetime.utcnow()):
+    db.session.add(User(id=Identity, username= UserID, email = ElectronicMail,datetimer=Dated))
     db.session.commit()
 
 @app.route("/")
@@ -44,9 +38,15 @@ def addRegion():
     full = request.form['projectFilePath']
     ment = request.form['email1']
     fill = request.form['username1']
+    # dud = request.form['datetime1']
     dataentry(full,fill,ment)
-    fullfillment = (full+"\n" + fill+"\n" +ment)
-    alltogether=User.query.all()
+    tester=User.query.filter_by(id=full).first()
+
+    return render_template('regionaloutput.html', alltogether=tester)
+@app.route('/data')
+def data():
+    alltogether=User.query.all() 
+
     return render_template('output.html', alltogether=alltogether)
 
 if __name__== '__main__':
